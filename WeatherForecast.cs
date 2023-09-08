bbffea22e1fa83b0d2a4f6bfbb7bc5d12a34884c
Jr Bytes <itamazor@gmail.com>
2023-09-07 22:54:43 -0300

    :tada: initial commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace SrsApi;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
